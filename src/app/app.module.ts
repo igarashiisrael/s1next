d1baa8ab5150873eccfc4e305b6fe7d57e562547
Israel <igarashiisrael@icloud.com>
2022-12-08 13:18:38 -0300

    angular materials adicinado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { EvolucaoFisioComponent } from './evolucao-fisio/evolucao-fisio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { EvolucaoFisioComponent } from './evolucao-fisio/evolucao-fisio.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
