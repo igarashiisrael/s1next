80347d9f93026613d103eb4bc8b8f9cb6a7320dc
Israel <igarashiisrael@icloud.com>
2022-12-08 09:25:38 -0300

    Componente Evo-Fisio adicionado

---
+++ src/app/evolucao-fisio/evolucao-fisio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evolucao-fisio',
+  templateUrl: './evolucao-fisio.component.html',
+  styleUrls: ['./evolucao-fisio.component.css']
+})
+export class EvolucaoFisioComponent {
+
+}
